8be8806e31c172429a26846d3dcb2bd4abd5d505
Johan Velandia <77521649+DragonVlx@users.noreply.github.com>
2021-01-21 23:37:57 -0500

    Second Proyect

---
+++ main.js
@@ -0,0 +1,10 @@
+
+/**/
+document.querySelector(".menu-btn").addEventListener ("click" , () => {
+    document.querySelector(".nav-menu").classList.toggle('show')
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.showcase',{delay:500});
+ScrollReveal().reveal('.Infocards',{delay:500});
+ScrollReveal().reveal('.cards-banner-one',{delay:500});
